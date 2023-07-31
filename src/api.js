0d1e99b5fef9c3fa82393e54ac04f4bb1d51250c
Diego <diego.boost@gmail.com>
2023-07-31 12:36:25 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '489c8b9e90msha55df0c689e7839p18633ajsn7ad0a665a859',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const OW_API_KEY = '653a1711bce0de22442859cde7c18e8f'
